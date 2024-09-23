206b73a448330b55731c246ef1a02d8360cb8781
seba.przygoda <seba.przygoda@gmail.com>
2024-09-23 21:09:14 +0200

    all lights working, debug mode implemented

--- Tank126p/src/main.cpp
+++ Tank126p/src/main.cpp
@@ -3,10 +3,15 @@
 #include <SPI.h>
 #include <AsyncDelay.h>
 
+#define IS_ON 0
+#define IS_OFF 1
 RH_ASK driver(4000, D0, D4, D4, false);
 
 const short blinkersLed_l_out = D5;
 const short blinkersLed_r_out = D6;
+const short headlights_pwm = D2;
+const short rear_lights_led = D1;
+
 
 int l_x;
 int l_y;
@@ -14,18 +19,21 @@ int r_x;
 int r_y;
 int l_b;
 int r_b;
+int red_b;
+int black_b;
+int lights_on_switch;
+int lights_mode_switch;
+int blinkers_mode_switch;
 
-char *decodedStringValues[7];
+char *decodedStringValues[11];
 char *ptr = NULL;
 byte decodingIndex = 0;
 
 AsyncDelay delay_blinkers;
 bool blinkersState = false;
-int isLeftBlinkerOff = 0;
-int isRightBlinkerOff = 0;
 
 void decodeRadioMessage(){
-    uint8_t buf[26];
+    uint8_t buf[34];
     uint8_t buflen = sizeof(buf);
     if (driver.recv(buf, &buflen))
     {
@@ -44,10 +52,20 @@ void decodeRadioMessage(){
       r_y = atoi(decodedStringValues[3]);
       l_b = atoi(decodedStringValues[4]);
       r_b = atoi(decodedStringValues[5]);
+      red_b = atoi(decodedStringValues[6]);
+      black_b = atoi(decodedStringValues[7]);
+      lights_on_switch = atoi(decodedStringValues[8]);
+      lights_mode_switch = atoi(decodedStringValues[9]);
+      blinkers_mode_switch = atoi(decodedStringValues[10]);
 
-      Serial.printf("Left X: %d, Left Y: %d, Left button: %d, Right X: %d, Right Y: %d, Right button: %d", l_x, l_y, l_b, r_x, r_y, r_b);
-      Serial.println();
     }
+}
+
+void debug(){
+  if (black_b == IS_ON){
+      Serial.printf("Left X: %d, Left Y: %d, Left button: %d, Right X: %d, Right Y: %d, Right button: %d, Red button: %d, Black button: %d, Lights: %d, Lights_Mode: %d, BlinkersMode: %d" , l_x, l_y, l_b, r_x, r_y, r_b, red_b, black_b, lights_on_switch, lights_mode_switch, blinkers_mode_switch);
+      Serial.println();
+  }
 }
 
 void blinkers(){
@@ -57,30 +75,48 @@ void blinkers(){
       delay_blinkers.repeat();
     }
       
-    isLeftBlinkerOff = l_b == 1;
-    isRightBlinkerOff = r_b == 1;
-
-    if (isLeftBlinkerOff && isRightBlinkerOff)
+    if (blinkers_mode_switch == 0)
     {
       digitalWrite(blinkersLed_l_out, LOW); 
       digitalWrite(blinkersLed_r_out, LOW);
     }
-    else if (isLeftBlinkerOff)
-    {
-      digitalWrite(blinkersLed_r_out, blinkersState);
-      digitalWrite(blinkersLed_l_out, LOW);
-    }
-    else if (isRightBlinkerOff)
+    else if (blinkers_mode_switch == 1)
     {
       digitalWrite(blinkersLed_l_out, blinkersState);
       digitalWrite(blinkersLed_r_out, LOW);
     }
+    else if (blinkers_mode_switch == 2)
+    {
+      digitalWrite(blinkersLed_r_out, blinkersState);
+      digitalWrite(blinkersLed_l_out, LOW);
+    }
+}
+
+void headlights(){
+  if (lights_on_switch == IS_ON){
+    analogWrite(headlights_pwm, lights_mode_switch == IS_ON ? 255 : 45);
+  }
+  else{
+    analogWrite(headlights_pwm, 0);
+  }
+}
+
+void rear_lights(){
+  if (lights_on_switch == IS_ON){
+    digitalWrite(rear_lights_led, HIGH);
+  }
+  else{
+    digitalWrite(rear_lights_led, LOW);
+  }
 }
 
 void setup() {
   Serial.begin(9600);
   pinMode(blinkersLed_l_out, OUTPUT);
   pinMode(blinkersLed_r_out, OUTPUT);
+  pinMode(headlights_pwm, OUTPUT);
+  pinMode(rear_lights_led, OUTPUT);
+  
   delay_blinkers.start(500, AsyncDelay::MILLIS);
 
   while(!Serial){
@@ -100,4 +136,7 @@ void setup() {
 void loop() {
   decodeRadioMessage();
   blinkers();
+  headlights();
+  rear_lights();
+  debug();
 }
--- Tank126pJoystick/src/main.cpp
+++ Tank126pJoystick/src/main.cpp
@@ -9,7 +9,13 @@ const short r_sensorX = A2;
 const short r_sensorY = A3;
 const short l_button = 9; 
 const short r_button = 10; 
-const short button3 = 11;
+const short red_button = 11;
+const short black_button = 8;
+const short blinkers_left_switch = 5;
+const short blinkers_right_switch = 6;
+const short headlights_on_switch = 4;
+const short headlights_mode_switch = 3;
+
 
 const short LOOSE_PIN = 0;
 
@@ -20,16 +26,21 @@ const short R_Y = 511;
 
 const short TRANSMITTER_PIN = 7;
 
-
 short lxval = 0;
 short lyval = 0;
 short rxval = 0;
 short ryval = 0;
 short lbval = 0;
 short rbval = 0;
-short bval = 0;
+short red_bval = 0;
+short black_bval = 0;
+short lights_on_val = 0;
+short lights_mode_val = 0; // 0 = short, 1 = long
+short blinkers_left_val = 0;
+short blinkers_right_val = 0;
+short blinkers_val = 0;
 
-char buffer[26];
+char buffer[34];
 RH_ASK rfdriver(4000, LOOSE_PIN, TRANSMITTER_PIN, LOOSE_PIN, false);
 
 void setup() {
@@ -45,18 +56,20 @@ void setup() {
 
   pinMode(l_button, INPUT_PULLUP);
   pinMode(r_button, INPUT_PULLUP);
-  pinMode(button3, INPUT_PULLUP);
-  pinMode(3, OUTPUT);
+  pinMode(red_button, INPUT_PULLUP);
+  pinMode(black_button, INPUT_PULLUP);
+  pinMode(blinkers_left_switch, INPUT_PULLUP);
+  pinMode(blinkers_right_switch, INPUT_PULLUP);
+  pinMode(headlights_on_switch, INPUT_PULLUP);
+  pinMode(headlights_mode_switch, INPUT_PULLUP);
   Serial.begin(9600);
 }
 
 void send (char *message)
 {
-  analogWrite(3, 50);
   Serial.println(message);
   rfdriver.send((uint8_t *)message, strlen(message));
   rfdriver.waitPacketSent(); 
-  analogWrite(3, 200);
 }
 
 void loop() {
@@ -68,9 +81,17 @@ void loop() {
 
   lbval = digitalRead(l_button);
   rbval = digitalRead(r_button);
-  bval = digitalRead(button3);
+  red_bval = digitalRead(red_button);
+  black_bval = digitalRead(black_button);
+  blinkers_left_val = digitalRead(blinkers_left_switch);
+  blinkers_right_val = digitalRead(blinkers_right_switch);
+  lights_on_val = digitalRead(headlights_on_switch);
+  lights_mode_val = digitalRead(headlights_mode_switch);
+  blinkers_val = blinkers_left_val == 0 ? 1 : blinkers_right_val == 0 ? 2 : 0; // 1 = left, 2 = right, 0 = off;
 
-  sprintf(buffer, "%d#%d#%d#%d#%d#%d#%d#",lxval,lyval,rxval,ryval,lbval,rbval,bval);
+
+  sprintf(buffer, "%d#%d#%d#%d#%d#%d#%d#%d#%d#%d#%d#", lxval, lyval, rxval, ryval, lbval, rbval, red_bval, black_bval, lights_on_val, lights_mode_val, blinkers_val);
+  Serial.println((char *) buffer);
 
   send(buffer);
 }
