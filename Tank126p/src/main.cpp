c00f5e1b46e4b919a962b9d9fd64f0ec118b5529
seba.przygoda <seba.przygoda@gmail.com>
2024-09-23 15:57:55 +0200

    blink blinkers with joystick buttons yay

--- Tank126p/src/main.cpp
+++ Tank126p/src/main.cpp
@@ -1,18 +1,87 @@
 #include <Arduino.h>
 #include <RH_ASK.h>
 #include <SPI.h>
+#include <AsyncDelay.h>
 
 RH_ASK driver(4000, D0, D4, D4, false);
 
-int DEBUG_LED = D5;
-int i = 0;
-String str_out;
-String str_l_x;
-String str_l_y;
+const short blinkersLed_l_out = D5;
+const short blinkersLed_r_out = D6;
+
+int l_x;
+int l_y;
+int r_x;
+int r_y;
+int l_b;
+int r_b;
+
+char *decodedStringValues[7];
+char *ptr = NULL;
+byte decodingIndex = 0;
+
+AsyncDelay delay_blinkers;
+bool blinkersState = false;
+int isLeftBlinkerOff = 0;
+int isRightBlinkerOff = 0;
+
+void decodeRadioMessage(){
+    uint8_t buf[26];
+    uint8_t buflen = sizeof(buf);
+    if (driver.recv(buf, &buflen))
+    {
+      decodingIndex = 0;
+      ptr = strtok((char *)buf, "#");
+      while(ptr != NULL)
+      {
+          decodedStringValues[decodingIndex] = ptr;
+          decodingIndex++;
+          ptr = strtok(NULL, "#"); 
+      }
+
+      l_x = atoi(decodedStringValues[0]);
+      l_y = atoi(decodedStringValues[1]);
+      r_x = atoi(decodedStringValues[2]);
+      r_y = atoi(decodedStringValues[3]);
+      l_b = atoi(decodedStringValues[4]);
+      r_b = atoi(decodedStringValues[5]);
+
+      Serial.printf("Left X: %d, Left Y: %d, Left button: %d, Right X: %d, Right Y: %d, Right button: %d", l_x, l_y, l_b, r_x, r_y, r_b);
+      Serial.println();
+    }
+}
+
+void blinkers(){
+  if(delay_blinkers.isExpired())
+    {
+      blinkersState = !blinkersState;
+      delay_blinkers.repeat();
+    }
+      
+    isLeftBlinkerOff = l_b == 1;
+    isRightBlinkerOff = r_b == 1;
+
+    if (isLeftBlinkerOff && isRightBlinkerOff)
+    {
+      digitalWrite(blinkersLed_l_out, LOW); 
+      digitalWrite(blinkersLed_r_out, LOW);
+    }
+    else if (isLeftBlinkerOff)
+    {
+      digitalWrite(blinkersLed_r_out, blinkersState);
+      digitalWrite(blinkersLed_l_out, LOW);
+    }
+    else if (isRightBlinkerOff)
+    {
+      digitalWrite(blinkersLed_l_out, blinkersState);
+      digitalWrite(blinkersLed_r_out, LOW);
+    }
+}
 
 void setup() {
   Serial.begin(9600);
-  pinMode(DEBUG_LED, OUTPUT);
+  pinMode(blinkersLed_l_out, OUTPUT);
+  pinMode(blinkersLed_r_out, OUTPUT);
+  delay_blinkers.start(500, AsyncDelay::MILLIS);
 
   while(!Serial){
     delay(1);
@@ -24,19 +93,11 @@ void setup() {
   }
   else
   {
-    Serial.println("RF init succeeded");
+    Serial.println("RF receiver init succeeded");
   }
 }
 
 void loop() {
-    uint8_t buf[26];
-    uint8_t buflen = sizeof(buf);
-    analogWrite(DEBUG_LED, 0);
-    if (driver.recv(buf, &buflen))
-    {
-      driver.printBuffer("Got: ", buf, buflen);
-      analogWrite(DEBUG_LED, 0);
-      Serial.println((char *)buf);
-      analogWrite(DEBUG_LED, 255);
-    }
-  }
+  decodeRadioMessage();
+  blinkers();
+}
